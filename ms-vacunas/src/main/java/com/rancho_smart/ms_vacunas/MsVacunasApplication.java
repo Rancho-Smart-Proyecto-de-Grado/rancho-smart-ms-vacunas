eca0718e50185e025d542df575fd671c09ce4b09
el-sebas-galeano <jsebastian-galeano@javeriana.edu.co>
2024-10-12 02:48:31 -0500

    :sparkles: feat: Creación de la Aplicación SpringBoot.

---
+++ ms-vacunas/src/main/java/com/rancho_smart/ms_vacunas/MsVacunasApplication.java
@@ -0,0 +1,13 @@
+package com.rancho_smart.ms_vacunas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsVacunasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsVacunasApplication.class, args);
+	}
+
+}
